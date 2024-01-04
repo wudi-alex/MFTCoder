03e5a5b86994cfd169803fa1b0238934bd7077ef
dw <1798827828@qq.com>
2024-01-03 22:26:02 -0500

    fix

---
+++ mft_peft_hf/src/pefts/merge.sh
@@ -0,0 +1,5 @@
+python merge_base_and_lora_to_hf.py \
+    --model_path codellama/CodeLlama-7b-hf \
+    --lora_adapter /projects/ksun3/dwu25/repairllama/repairllama-lora \
+    --save_path /projects/ksun3/dwu25/trained_models/repairllama \
+    --model_type llama
